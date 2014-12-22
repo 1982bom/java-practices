88525f0e35c17a8971e178c2f7850c6e2fc02035
1982bom <1982bom@gmail.com>
2014-12-22 22:21:56 +0900

    Add IntegerOverflow.java
     class for checking integer overflow
    
    Signed-off-by: 1982bom <1982bom@gmail.com>

--- IntegerOverflow.java
+++ IntegerOverflow.java
@@ -8,8 +8,27 @@ public class IntegerOverflow {
 		return i0 + i1;
 	}
 
+	public static int sub(int i0, int i1) throws ArithmeticException {
+		if (i1 > 0 && i0 < Integer.MIN_VALUE + i1)
+			throw new ArithmeticException("integer overflow");
+		else if (i1 < 0 && i0 > Integer.MAX_VALUE + i1)
+			throw new ArithmeticException("integer overflow");
+		return i0 - i1;
+	}
+
+	public static int multi(int i0, int i1) throws ArithmeticException {
+		long l = (long)i0 * i1;
+		if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE)
+			throw new ArithmeticException("integer overflow");
+		return i0 * i1;
+	}
+
+	public static int div(int i0, int i1) throws ArithmeticException {
+		if (i0 == Integer.MIN_VALUE && i1 == -1)
+			throw new ArithmeticException("integer overflow");
+		return i0 / i1;
+	}
+
 	public static void main(String[] args) {
-		//add(Integer.MAX_VALUE, 1);
-		add(Integer.MIN_VALUE, -1);
 	}
 }
--- TestIntegerOverflow.java
+++ TestIntegerOverflow.java
@@ -1,9 +1,44 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import junit.framework.TestCase;
 
-public class TestIntegerOverflow {
+public class TestIntegerOverflow extends TestCase {
 	public void testAdd() {
-		assertEquals(true, true);
+		try {
+			IntegerOverflow.add(Integer.MAX_VALUE, 1);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
+		try {
+			IntegerOverflow.add(Integer.MIN_VALUE, -1);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
+	}
+
+	public void testSub() {
+		try {
+			IntegerOverflow.sub(Integer.MIN_VALUE, 1);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
+		try {
+			IntegerOverflow.sub(Integer.MAX_VALUE, -1);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
+	}
+
+	public void testMulti() {
+		try {
+			IntegerOverflow.multi(Integer.MIN_VALUE, Integer.MIN_VALUE);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
+		try {
+			IntegerOverflow.multi(Integer.MIN_VALUE, Integer.MAX_VALUE);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
+	}
+
+	public void testDiv() {
+		try {
+			IntegerOverflow.div(Integer.MIN_VALUE, -1);
+			assertTrue(false);
+		} catch (ArithmeticException e) {}
 	}
 }
 
